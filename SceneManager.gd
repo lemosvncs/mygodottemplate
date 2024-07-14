0e3995d9d6f19884bd96f0f7b2be6f06019710aa
Vini Lemos <lemos.vncs@gmail.com>
2024-07-14 14:59:24 -0300

    Add files via upload

---
+++ SceneManager.gd
@@ -0,0 +1,39 @@
+extends Node
+
+var states = States.Screens
+
+var current_state:States.Screens
+
+@onready var screen_scenes:Dictionary = {
+	states.INTRO: $Intro,
+	states.MAIN_MENU: $MainMenu,
+	states.PAUSE_MENU: $PauseMenu,
+	states.GAME: $Game,
+	states.GAME_OVER: $GameOver,
+	states.CREDITS: $Credits,
+}
+
+func _ready():
+	current_state = states.MAIN_MENU
+	set_screen_visibility(current_state)
+	
+func set_screen_visibility(new_state):
+	for screen in screen_scenes:
+		if screen_scenes[screen] != screen_scenes[states.GAME] and screen_scenes[screen] != screen_scenes[new_state]:
+			screen_scenes[screen].visible = false
+	screen_scenes[new_state].visible = true
+
+func change_state(new_state):
+	match new_state:
+		states.INTRO:
+			set_screen_visibility(new_state)
+		states.MAIN_MENU:
+			set_screen_visibility(new_state)
+		states.PAUSE_MENU:
+			set_screen_visibility(new_state)
+		states.GAME:
+			set_screen_visibility(new_state)
+		states.GAME_OVER:
+			set_screen_visibility(new_state)
+		states.CREDITS:
+			set_screen_visibility(new_state)
---
+++ States.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name States
+
+enum Screens {
+	INTRO,
+	MAIN_MENU,
+	PAUSE_MENU,
+	GAME,
+	GAME_OVER,
+	CREDITS,
+}
